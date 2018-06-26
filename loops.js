717ba7fa3e2c896cf595f385212e090e55b40514
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-26 17:16:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,4 +4,26 @@ function forLoop(array)
   {
     array[i] = "I am ${i} strange loops."
   }
+
+
+function whileLoop(number)
+{
+  while (var i < number )
+  {
+    console.log(--number)
+  }
+  return "done"
+}
+
+function maybeTrue() 
+{
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array)
+{
+  do 
+  {
+    
+  } while (maybeTrue())
 }
